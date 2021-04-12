ce359d710d2098070c4c491e3242ca2fa3cfa3d8
WhyDias <diasmaidanbek@gmail.com>
2021-04-12 14:49:44 +0600

    'firstCommit'

---
+++ lib/services/auth.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //sign in anon
+  Future signInAnon() async {
+    try {
+      // result = await _auth.signInAnonymously();
+
+    } catch (e) {}
+  }
+  //sign in with mail n pass
+
+  //reg in with mail n pass
+
+  //sign out
+
+}
